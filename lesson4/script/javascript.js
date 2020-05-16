01e0d243ec0f7cc9d217a60ffff0f4e56c44289b
jqueen3 <que19002@byu.edu>
2020-05-15 23:10:22 -0700

    Update

---
+++ lesson4/script/javascript.js
@@ -0,0 +1,11 @@
+let copyyear = new Date();
+
+document.getElementById("year").textContent = copyyear.getFullYear();
+
+let modified = document.lastModified;
+
+document.getElementById("date").textContent = modified;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classlist.toggle("hide");
+}
